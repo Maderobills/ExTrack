211a495b5fde70c0765d981080f5b2ca98be4a21
Maderobills <96554416+Maderobills@users.noreply.github.com>
2023-03-09 01:06:25 +0000

    Home layout 88 nav 88 bottomnav

--- app/src/main/java/com/example/extrack/Home.java
+++ app/src/main/java/com/example/extrack/Home.java
@@ -1,14 +1,90 @@
 package com.example.extrack;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class Home extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar=findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Expense Tracker");
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView=findViewById(R.id.naView);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void displaySelectedListener(int itemId) {
+
+        Fragment fragment = null;
+
+        switch (itemId) {
+            case R.id.dashboard:
+                //fragment = new DashboardFragment();
+                break;
+
+            case R.id.income:
+               // fragment = new IncomeFragment();
+                break;
+
+            case R.id.expense:
+               // fragment = new ExpenseFragment();
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_frame, fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
